4cf17651db6dd592819c9b01e70d7869b240957f
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-09-18 18:55:30 +0000

    Creating setup.py and some documentation basis
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.common/trunk@205 82af7df8-bc4b-4ebc-8022-2999806f7efb

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+name = 'sd.common'
+path = name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+readme = open("README.txt").read()
+history = open(join('docs', 'HISTORY.txt')).read().replace(name + ' - ', '')
+
+setup(name = name,
+      version = version,
+      description = 'Structured Document Common Library',
+      long_description = readme[readme.find('\n\n'):] + '\n' + history,
+      keywords = 'plone zope structureddocument',
+      author = 'Souheil Chelfouh',
+      author_email = 'souheil@chelfouh.com',
+      url = 'http://tracker.trollfot.org/wiki/StructuredDocument',
+      download_url = 'http://pypi.python.org/pypi/sd.common',
+      license = 'GPL',
+      packages = find_packages(),
+      namespace_packages = ['sd'],
+      include_package_data = True,
+      platforms = 'Any',
+      zip_safe = False,
+      install_requires=[
+      ],
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+)
