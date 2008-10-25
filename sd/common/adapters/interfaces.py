cf3a2c87bfbb4ecca82b5ee8a53ec2d9df81dcaf
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-10-25 00:22:27 +0000

    Corrected for dynamic layout refactoring.
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.common/trunk@326 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/common/adapters/interfaces.py
+++ sd/common/adapters/interfaces.py
@@ -7,7 +7,7 @@ class IContentQueryHandler(Interface):
     """This interface defines an adapter used in order to fetch the
     very content of a folder-ish type, such as Folder, Topics, etc.
     """
-    def query_contents(show_inactive=False, contentFilter=None):
+    def query_contents(show_inactive=False, limit=None, **contentFilter):
         """
         """
         
